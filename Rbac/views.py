4e61bbb9b682d57d4a6396daad155b173afcb16d
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-09-05 10:41:09 +0800

    测试登录接口

--- Rbac/views.py
+++ Rbac/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
 from Rbac.models import *
+from django.http import JsonResponse
 
 
 class AuthView(APIView):
     def post(self, request):
         print(request.data)
+        return JsonResponse(request.data)
 
     def get(self, request):
         print(request.data)
+        return JsonResponse(request.data)

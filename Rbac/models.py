56bf634537cf823199796585776a8a618edbc8f2
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-09-05 11:13:56 +0800

    测试登录接口

--- Rbac/models.py
+++ Rbac/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+# from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
 

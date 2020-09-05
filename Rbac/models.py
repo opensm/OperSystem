b425e4ced3f2ac19c4cd1da845a190ef8724b6e2
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-09-05 11:16:32 +0800

    测试登录接口

--- Rbac/models.py
+++ Rbac/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-# from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import UserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
 
@@ -57,6 +57,7 @@ class UserInfo(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['mobile', 'department', 'is_active', 'is_superuser', 'roles']
 
+    objects = UserManager()
     class Meta:
         verbose_name_plural = _("User")
 

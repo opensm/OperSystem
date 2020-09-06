cd4d08f325e03eec66cb353b04092e97c29d5223
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-09-06 11:43:51 +0800

    添加角色管理接口

--- Rbac/views.py
+++ Rbac/views.py
@@ -135,9 +135,9 @@ class RoleView(APIView):
             return JsonResponse(res)
         else:
             data = ret.update(instance=query, validated_data=ret.validated_data)
-            result = serializers.serialize('json', data)
+            # result = serializers.serialize('json', data)
             res = {
-                "data": result,
+                "data": request.data,
                 "meta": {"msg": "修改角色信息成功", "status": 200}
             }
         return JsonResponse(res)

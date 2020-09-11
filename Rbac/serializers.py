2f5932091486d8f6d82bab1ac4fa8ed1b7d6d4ad
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-09-11 18:46:55 +0800

    用户管理相关功能开发

--- Rbac/serializers.py
+++ Rbac/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework.serializers import Serializer, ModelSerializer
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 from django.contrib import auth
 from Rbac.models import Role, Permission, UserInfo, UserToken
 import hashlib
--- Rbac/views.py
+++ Rbac/views.py
@@ -2,7 +2,11 @@
 from rest_framework.views import APIView
 from Rbac.models import *
 from django.http import JsonResponse
-from Rbac.serializers import RoleSerializer, PermissionSerializer, SignInSerializer
+from Rbac.serializers import \
+    RoleSerializer, \
+    PermissionSerializer, \
+    SignInSerializer, \
+    UserInfoSerializer
 
 
 class AuthView(APIView):
@@ -21,6 +25,7 @@ class AuthView(APIView):
 
 
 class RolesView(APIView):
+
     def get(self, request):
         try:
             data = Role.objects.all()
@@ -232,5 +237,108 @@ class PermissionView(APIView):
             res = {
                 "data": "null",
                 "meta": {"msg": "删除权限失败:{0}".format(error), "status": 500}
+            }
+            return JsonResponse(res)
+
+
+class UsersView(APIView):
+
+    def get(self, request):
+        try:
+            data = UserInfo.objects.all()
+            ret = UserInfoSerializer(instance=data, many=True)
+            res = {
+                "data": ret.data,
+                "meta": {"msg": "获取角色成功", "status": 200}
+            }
+            return JsonResponse(res)
+        except Exception as error:
+            res = {
+                "data": "null",
+                "token": "null",
+                "meta": {"msg": "内部错误:{0}".format(error), "status": 500}
+            }
+            return JsonResponse(res)
+
+    def post(self, request):
+        res = UserInfoSerializer(data=request.data)
+        if not res.is_valid():
+            res = {
+                "data": "null",
+                "meta": {"msg": "传入参数错误:{0}".format(res.errors), "status": 500}
+            }
+            return JsonResponse(res)
+        else:
+            res.save()
+            data = {
+                "data": res.data,
+                "meta": {"msg": "角色数据保存成功", "status": 200}
+            }
+            return JsonResponse(data)
+
+
+class UserView(APIView):
+
+    def get(self, request, userId):
+        """
+        :param request:
+        :param userId:
+        :return: 查看具体角色信息
+        """
+        ret = UserInfoSerializer(UserInfo.objects.filter(id=userId).first())
+        # print(Role.objects.filter(id=roleId))
+        data = {
+            "data": ret.data,
+            "meta": {"msg": "查看角色信息成功", "status": 200}
+        }
+        return JsonResponse(data)
+
+    def post(self, request):
+        """
+        :param request:
+        :return:修改角色权限分配
+        """
+        return JsonResponse({"data": "post"})
+
+    def put(self, request, userId):
+        """
+        :param request:
+        :param userId:
+        :return: 修改角色信息
+        """
+        query = UserInfo.objects.filter(pk=userId).first()
+        ret = UserInfoSerializer(instance=query, data=request.data)
+        if not ret.is_valid():
+            res = {
+                "data": "null",
+                "meta": {"msg": "传入参数错误:{0}".format(ret.errors), "status": 500}
+            }
+            return JsonResponse(res)
+        else:
+            ret.update(instance=query, validated_data=ret.validated_data)
+            # result = serializers.serialize('json', data)
+            res = {
+                "data": ret.data,
+                "meta": {"msg": "修改角色信息成功", "status": 200}
+            }
+        return JsonResponse(res)
+
+    def delete(self, request, userId):
+        """
+        :param request:
+        :param userId:
+        :return: 删除角色
+        """
+        try:
+            UserInfo.objects.get(id=userId).delete()
+            res = {
+                "data": request.data,
+                "meta": {"msg": "删除信息成功！", "status": 200}
+            }
+            return JsonResponse(res)
+        except Exception as error:
+            res = {
+                "data": "null",
+                "meta": {"msg": "删除角色失败:{0}".format(error), "status": 500}
             }
             return JsonResponse(res)

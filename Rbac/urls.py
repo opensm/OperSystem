4cc82f1d346eb390efce41920bbcb54b173cf8d1
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-09-12 19:01:26 +0800

    用户管理相关功能开发

--- Rbac/urls.py
+++ Rbac/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
-from Rbac.views import AuthView, RoleView, RolesView, PermissionsView, PermissionView
+from Rbac.views import AuthView, RoleView, RolesView, PermissionsView, PermissionView, UserView, UsersView
 
 app_name = 'rbac'
 
 urlpatterns = [
     # 用户登录
     path('auth/login', AuthView.as_view(), name='login'),
-    # 查询角色
+    # 角色管理
     path('roles', RolesView.as_view(), name='roles'),
     path('role/<int:roleId>', RoleView.as_view(), name='role'),
+    # 权限管理
     path('permissions', PermissionsView.as_view(), name='permissions'),
     path('permission/<int:permissionId>', PermissionView.as_view(), name='permission'),
+    # 用户管理
+    path('users', UsersView.as_view(), name='users'),
+    path('user/<int:userId>', UserView.as_view(), name='user'),
 ]

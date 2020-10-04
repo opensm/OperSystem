bef9895d8cfcdd967d710d626ea36861ac0eaa72
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-10-04 10:28:58 +0800

    修改用户密码

--- Rbac/serializers.py
+++ Rbac/serializers.py
@@ -31,6 +31,8 @@ class PermissionSerializer(ModelSerializer):
             raise serializers.ValidationError("当权限为完整路径，则父权限应该为空")
         if path_length > 1 and attrs['level'] != 999:
             raise serializers.ValidationError("获取到权限为完整路径，权限等级应该为:999！")
+        if path_length < 1 and attrs['level'] == 999:
+            raise serializers.ValidationError("输入的权限等级，与权限不一致！")
         return attrs
 
     def validate_permission_type(self, attrs):

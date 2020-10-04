696fd0984deb5c504ee151d3cc92186748315c33
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-10-04 13:06:17 +0800

    修改用户密码

--- Rbac/urls.py
+++ Rbac/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from Rbac.views import AuthView, \
     RoleView, \
     RolesView, \
@@ -14,13 +14,13 @@ urlpatterns = [
     # 用户登录
     path('auth/login', AuthView.as_view(), name='login'),
     # 角色管理
-    path('roles', RolesView.as_view(), name='roles'),
+    re_path('role$', RolesView.as_view(), name='roles'),
     path('role/<int:roleId>', RoleView.as_view(), name='role'),
     # 权限管理
-    path('permissions', PermissionsView.as_view(), name='permissions'),
+    re_path('permission$', PermissionsView.as_view(), name='permissions'),
     path('permission/<int:permissionId>', PermissionView.as_view(), name='permission'),
     # 用户管理
-    path('users', UsersView.as_view(), name='users'),
+    re_path('user$', UsersView.as_view(), name='users'),
     path('user/<int:userId>', UserView.as_view(), name='user'),
     path('resetpass/<int:userId>', ResetPassWordView.as_view(), name='password'),
 ]

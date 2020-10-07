703abd9cb76bb191165d570191675a9656296384
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-10-07 12:09:27 +0800

    修改部分bug

--- Rbac/models.py
+++ Rbac/models.py
@@ -80,7 +80,10 @@ class UserInfo(AbstractBaseUser, PermissionsMixin):
 
 
 class UserToken(models.Model):
-    username = models.OneToOneField(to='UserInfo', on_delete=models.DO_NOTHING, verbose_name="用户")
+    username = models.OneToOneField(
+        to='UserInfo', on_delete=models.DO_NOTHING, verbose_name="用户",
+        to_field="username"
+    )
     token = models.CharField(max_length=60)
     update_date = models.DateTimeField(verbose_name='更新日期', auto_now_add=True)
     expiration_time = models.DateTimeField(verbose_name='失效时间', auto_now_add=True)

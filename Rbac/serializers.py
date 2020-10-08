c8eb4a053bd43520cbdff06c532dc9cf67a5d5b5
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-10-08 12:08:09 +0800

    修改部分bug

--- Rbac/serializers.py
+++ Rbac/serializers.py
@@ -167,7 +167,7 @@ class UserStatusEditSerializer(serializers.ModelSerializer):
 
 
 class RolePermissionEditSerializer(serializers.ModelSerializer):
-    permissions = serializers.PrimaryKeyRelatedField(many=True, required=True, read_only=True)
+    permissions = serializers.PrimaryKeyRelatedField(many=True, required=True, queryset=Permission.objects.all())
 
     class Meta:
         model = Role

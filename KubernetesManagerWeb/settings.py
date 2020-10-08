19ad96d82388ae7b719e8df81c821900ab551d5b
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-10-08 16:01:21 +0800

    修改部分bug

--- KubernetesManagerWeb/settings.py
+++ KubernetesManagerWeb/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Rbac.Middleware.RbacMiddlewareMixin.MiddlewareMixin'
+    'Rbac.Middleware.RbacMiddlewareMixin.RbacMiddleware'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True

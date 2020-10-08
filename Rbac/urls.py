b82a98da2030a682b06a069de05f140417114aaf
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-10-08 15:10:06 +0800

    修改部分bug

--- Rbac/urls.py
+++ Rbac/urls.py
@@ -18,15 +18,15 @@ urlpatterns = [
     path('login', AuthView.as_view(), name='login'),
     # 角色管理
     re_path('^role$', RolesView.as_view(), name='roles'),
-    re_path('role/(?P<roleId>[0-9])$', RoleView.as_view()),
-    re_path('role/(?P<roleId>[0-9])/permission$', RolePermissionEditView.as_view()),
+    re_path('^role/(?P<roleId>[0-9])$', RoleView.as_view()),
+    re_path('^role/(?P<roleId>[0-9])/permission$', RolePermissionEditView.as_view()),
     # 权限管理
-    re_path('permission$', PermissionsView.as_view(), name='permissions'),
+    re_path('^permission$', PermissionsView.as_view(), name='permissions'),
     path('permission/<int:permissionId>', PermissionView.as_view(), name='permission'),
     # 用户管理
-    re_path('user$', UsersView.as_view(), name='users'),
-    re_path('user/(?P<userId>[0-9])$', UserView.as_view(), name='user'),
-    re_path('user/(?P<userId>[0-9])/reset_passoword$', ResetPassWordView.as_view(), name='password'),
-    re_path('user/(?P<userId>[0-9])/state$', UserStatusEditView.as_view(), name="user_status"),
-    re_path('user/(?P<userId>[0-9])/roles$', UserEditRoleView.as_view(), name="user_role")
+    re_path('^user$', UsersView.as_view(), name='users'),
+    re_path('^user/(?P<userId>[0-9])$', UserView.as_view(), name='user'),
+    re_path('^user/(?P<userId>[0-9])/reset_passoword$', ResetPassWordView.as_view(), name='password'),
+    re_path('^user/(?P<userId>[0-9])/state$', UserStatusEditView.as_view(), name="user_status"),
+    re_path('^user/(?P<userId>[0-9])/roles$', UserEditRoleView.as_view(), name="user_role")
 ]

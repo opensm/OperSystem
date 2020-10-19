10f655c63f06d58bcc5e3837802c5f22276fad2c
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-10-19 14:41:23 +0800

    修改部分bug

--- Rbac/Middleware/RbacMiddlewareMixin.py
+++ Rbac/Middleware/RbacMiddlewareMixin.py
@@ -34,18 +34,13 @@ class RbacMiddleware(MiddlewareMixin):
             raise Exception("输入类型错误！")
         # 当前为页面并没有父页面
         if obj.parent is None and path is None:
-            print(1, obj.path)
             return obj.path
         # 当前为页面并之前也存在页面
         elif obj.parent is None and path is not None:
-            print(2, obj.path, path)
-            print(os.path.join(obj.path, path))
             return os.path.join(obj.path, path)
         elif obj.parent is not None and path is None:
-            print(3, obj.path)
             return self.format_url(obj=obj.parent, path=obj.path)
         elif obj.parent is not None and path is not None:
-            print(4)
             return self.format_url(obj.parent, path=os.path.join(obj.path, path))
         else:
             print(obj.parent, path)

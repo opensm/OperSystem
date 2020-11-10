f9fe260dbd6c9275db669231ee83f94c24f2c034
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-11-10 18:31:57 +0800

    修改部分bug

--- Rbac/views.py
+++ Rbac/views.py
@@ -73,12 +73,14 @@ class AuthView(APIView):
                 raise Exception("用户不存在：{0}".format(data.data['username']))
             user_obj = UserInfo.objects.get(username=data.data['username'])
             if not UserToken.objects.filter(username=user_obj).exists():
+                print("************{0}*****************".format(expiration_time))
                 UserToken.objects.create(
                     username=user_obj,
                     token=token,
                     expiration_time=expiration_time,
                     update_date=timezone.now()
                 )
+                print("============={0}================".format(timezone.now()))
             else:
                 user_token_obj = UserToken.objects.get(username=user_obj)
                 user_token_obj.token = token

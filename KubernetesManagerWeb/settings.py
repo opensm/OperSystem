46af753083406b68a069d8fb5f55dee1b6695257
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-11-12 09:53:51 +0800

    修改部分bug

--- KubernetesManagerWeb/settings.py
+++ KubernetesManagerWeb/settings.py
@@ -115,11 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'guardian.backends.ObjectPermissionBackend',
-)
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 

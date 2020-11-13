24bf9834cfd8e8f945b6e28de8d27bd4f35c0927
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-11-13 15:53:15 +0800

    修改部分bug

--- Rbac/views.py
+++ Rbac/views.py
@@ -793,6 +793,7 @@ class CurrentUser(APIView):
         menu = data.data
         # menu['roles'] = user.get_menu(user_obj=token_object.username)
         menu['user_permissions'] = user.get_menu(user_obj=token_object.username)
+        print(menu)
         res = {
             "data": menu,
             "meta": {"msg": "获取当前用户信息成功！", "status": 200}

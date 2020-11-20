f254f4da9175d19ab31765898b91651f6d90c7dd
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-11-20 13:29:23 +0800

    修改部分bug

--- Rbac/Middleware/ExceptionMiddleware.py
+++ Rbac/Middleware/ExceptionMiddleware.py
@@ -15,7 +15,6 @@ class ExceptionBoxMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
         if not issubclass(exception.__class__, BaseReturn):
             return None
-        print(exception)
         ret_json = {
             'code': exception.__class__.__name__,
             'message': getattr(exception, 'message', ''),

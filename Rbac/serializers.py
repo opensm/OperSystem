2de1b561080ea08b18511361f5289e47a36cf39c
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2021-01-18 18:18:39 +0800

    修改部分bug

--- Rbac/serializers.py
+++ Rbac/serializers.py
@@ -28,7 +28,7 @@ class PermissionSerializer(serializers.ModelSerializer):
     class Meta:  # 如果不想每个字段都自己写，那么这就是固定写法，在继承serializer中字段必须自己写，这是二者的区别
         model = Permission  # 指定需要序列化的模型表
         fields = ("__all__")
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'parent']
 
     def validate_parent(self, attrs):
         """

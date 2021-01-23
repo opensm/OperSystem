cb5b9d44328c70334d0165fc87c916ba36559a70
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2021-01-23 14:36:01 +0800

    修改部分bug

--- Rbac/serializers.py
+++ Rbac/serializers.py
@@ -23,7 +23,7 @@ class SubPermissionSerializer(serializers.ModelSerializer):
 
 class PermissionSerializer(serializers.ModelSerializer):
     children = RecursiveField(many=True, read_only=True, allow_null=True)
-    parent = serializers.PrimaryKeyRelatedField(allow_null=True)
+    parent = serializers.PrimaryKeyRelatedField()
 
     def validate_parent(self, attrs):
         """

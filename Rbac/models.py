f2f7b61f74220df69e7e015988a149225ca79893
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2021-02-18 15:21:18 +0800

    修改部分bug

--- Rbac/models.py
+++ Rbac/models.py
@@ -111,9 +111,6 @@ class DataPermissionList(models.Model):
     value = models.CharField(verbose_name="权限值对应的列表", default="", max_length=20)
     request_type = models.ManyToManyField(RequestType, verbose_name="请求类型", default=0)
     role = models.ForeignKey(Role, default=None, on_delete=models.DO_NOTHING)
-    data_check_type = models.CharField(
-        verbose_name="校验数据权限类型", max_length=10, default='pk', choices=check_type
-    )
     object_id = models.PositiveIntegerField(default=0)
     check_field = models.CharField(verbose_name="校验的字段", max_length=20, default="pk", null=True)
 

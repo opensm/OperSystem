5adfa2351acf64dfe0cbd87281e439902dad1fa1
engine-oper <smxblysq@qq.com>
2021-04-22 15:37:52 +0800

    修改部分代码

--- lib/views.py
+++ lib/views.py
@@ -126,6 +126,7 @@ class BasePUTVIEW(DataQueryPermission, APIView, RewritePageNumberPagination):
                     code=API_40003_PERMISSION_DENIED
                 )
             model_objs = self.get_user_data_objects(request=request)
+            print(model_objs)
             if not model_objs or len(model_objs) > 1:
                 raise APIException(detail="获取到修改数据异常，请检查！", code=API_12001_DATA_NULL_ERROR)
             data = self.serializer_class(

56519a4b2c9436f0f3dd3d226323b1a20f60f3aa
engine-oper <smxblysq@qq.com>
2021-05-12 21:32:00 +0800

    修改Bug

--- Rbac/serializers.py
+++ Rbac/serializers.py
@@ -21,7 +21,7 @@ class RequestTypeSerializer(serializers.ModelSerializer):
 class ContentTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContentType
-        fields = ('app_label', 'model')
+        fields = ("__all__")
 
 
 class DataPermissionSerializer(serializers.ModelSerializer):

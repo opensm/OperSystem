435499775b09ea68e606f694257fb68d360dd403
engine-oper <smxblysq@qq.com>
2021-05-20 19:03:46 +0800

    修改Bug

--- lib/views.py
+++ lib/views.py
@@ -338,13 +338,13 @@ class UserGETView(DataQueryPermission, APIView):
                 detail="输入类型错误！"
             )
         for x in menu_list:
-            print(x.parent, x.children,x.name)
+            print(x.parent, x.children, x.name)
             if not isinstance(x, Menu):
                 raise APIException(
                     code=API_50001_SERVER_ERROR,
                     detail="输入类型错误！"
                 )
-            if not x.children and x.parent:
+            if x.parent:
                 print("11111111111111111111111")
                 try:
                     data = SubMenuSerializer(instance=x)
@@ -357,14 +357,11 @@ class UserGETView(DataQueryPermission, APIView):
                     "{}".format(x.pk),
                     []
                 ).extend(data.data)
-            elif not x.children and not x.parent:
+            elif not x.parent:
                 print("11111111111111111111112")
                 menu.append(
                     SubMenuSerializer(instance=x).data
                 )
-            else:
-                print("11111111111111111111113")
-                continue
         for key, value in menu_dict.items():
             obj = Menu.objects.get(id=key)
             try:

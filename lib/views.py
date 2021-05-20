c5653efcc2b1ae848b787a11d489df609e4b1ef6
engine-oper <smxblysq@qq.com>
2021-05-20 19:12:56 +0800

    修改Bug

--- lib/views.py
+++ lib/views.py
@@ -319,9 +319,7 @@ class UserGETView(DataQueryPermission, APIView):
             data = MenuSerializer(instance=instance, many=True)
             return data.data
         else:
-            print(self.user.roles.menu.all())
             data = self.get_user_menu(menu_list=self.user.roles.menu.all())
-            print(data)
         return data
 
     def get_user_menu(self, menu_list):

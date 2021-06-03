074be3802be3c863d9ed3deec3ba04c37a51ac6c
engine-oper <smxblysq@qq.com>
2021-06-03 17:22:58 +0800

    创建模型

--- Task/management/commands/run.py
+++ Task/management/commands/run.py
@@ -14,7 +14,6 @@ class Command(BaseCommand):
                     status__in=['approveing', 'not_start_approve', 'ok_approved']
             ):
                 if not self.checkTaskStatus(task=data):
-                    RecodeLog.warn(msg='任务还未准备OK:{}:{}'.format(data.id, data.name))
                     continue
                 RecodeLog.info(msg='即将开始任务:{}:{}'.format(data.id, data.name))
                 if not self.runTask(task=data):
